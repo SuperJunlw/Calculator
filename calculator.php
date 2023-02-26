06de89809a177977d29ed8a0fc371271fc0240fe
Jun Wu <60298jw@gmail.com>
2023-02-25 22:18:45 -0800

    adding the javaScript for vaildating input 1

--- calculator.php
+++ calculator.php
@@ -2,10 +2,26 @@
 <html>
     <head>
         <title>CS174-Assignment 2 Calculator</title>
+        <style>
+            p#error { color: red; }
+        </style>
     </head>
     <body>
+        <script type="application/javascript">
+	        function validateInput1() {
+                let input = document.getElementById("input1").value;;
+                //check if the input is empty of not a number
+                if (input == '' || isNaN(input)) {
+		            document.getElementById("error").innerHTML = "Input 1 is missing or not a valid number";//write the error message
+		            return false;
+                }
+                return true;
+	        }
+        </script>
+
         <h1>Calculator</h1>
-        <form action = "calculator.php" method = "post">
+        <!-- Creating the form, call validateInput1() everytime that calculate is clicked -->
+        <form action = "calculator.php" onsubmit="return validateInput1();" method = "post">
             <p>Input 1: <input type="text" name="input1" id="input1"/><label for="textfield" class="left"></label></p>
             <p>Operation: <select name="operator" id="operator">
                 <option value="" disabled selected hidden>Operator</option>
@@ -19,41 +35,40 @@
         </form>
         <hr />
 
-        <?php if (!empty($_POST)){
-            $isVaild = true;
-            if(!is_numeric($_POST["input1"]) ){
-                ?>
-                <p style="color:red;">Input 1 is missing or not a valid number</p>
-                <?php
-                $isVaild = false;
-            }
+        <p id="error"></p>
 
+        <?php if (!empty($_POST)){
+            $isVaild = true; //a boolean to check if all inputs are vaild
+            //check if input 2 is vaild
             if(!is_numeric($_POST["input2"]) ){
                 ?>
                 <p style="color:red;">Input 2 is missing or not a valid number</p>
                 <?php
                 $isVaild = false;
             }
-
+            
+            //check if an operator is seleted
             if (empty($_POST["operator"])) {
                 ?>
-                <p style="color:red;"><?php echo "Operator is missing"; ?> </p>
+                <p style="color:red;">Operator is missing</p>
                 <?php
                 $isVaild = false;
             }
 
+            //if all inputs are vaild
             if($isVaild){
                 $num1 = $_POST["input1"];
                 $num2 = $_POST["input2"];
                 $op = $_POST["operator"];
                 $answer = 0;
 
+                //check divide by 0
                 if($op == "/" && $num2 == 0){
                 ?>
                     <p style="color:red;">Cannot divide 0</p>
                 <?php
                 }
-                else{
+                else{ //calculate the answer based on the operator
                     if($op == "+")
                         $sum = $num1 + $num2;
                     else if($op == "-")
@@ -62,8 +77,9 @@
                         $sum = $num1 * $num2;
                     else if($op == "/")
                         $sum = $num1 / $num2;
+                    //print the answer
                     ?>
-                    <p style="color:blue;"><?php echo $num1 . $op . $num2 . "=" . $sum; ?></p>
+                    <p style="color:blue;"><?php echo $num1 . $op . $num2 . "=" . $sum; ?></p> 
                 <?php
                 }
             }
